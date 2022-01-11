de09d4517ef91ef7c61bcf96e5ba5fc3e47b3d2b
Pranjal Patra <pranjal.patra.2022@gmail.com>
2022-01-11 15:03:51 -0330

    create label and entry

--- 1 GUIs/home.py
+++ 1 GUIs/home.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 # Create the root window
 root = tk.Tk()
 root.title('hi')
 root.geometry('400x200')
-root.mainloop()
 
+# Create the main frame
 frame_home = ttk.Frame(root)
 frame_home.pack(fill=tk.BOTH, expand=True)
+
+# Create a label
+ttk.Label(frame_home, text="Enter Name: ").grid(column=0, row=0)
+
+# Create an entry box
+string_user_name = tk.StringVar()
+entry_name = ttk.Entry(frame_home, width=30, textvariable=string_user_name).grid(column=1, row=0)
+
+root.mainloop()
