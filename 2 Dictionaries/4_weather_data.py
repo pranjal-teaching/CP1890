dfd47d6e561770220a70c8624dc20d50957db16a
Pranjal Patra <pranjal.cna@gmail.com>
2022-01-20 15:03:05 -0330

    add 4_weather_data.py

---
+++ 2 Dictionaries/4_weather_data.py
@@ -0,0 +1,16 @@
+import requests
+
+# construct URL to get weather info
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+city = "toronto"
+api_key = "e3a3f627f93effe0dc13ea5ca84180c7" # https://home.openweathermap.org/api_keys
+url = f'{base_url}q={city}&appid={api_key}'
+print(url)
+
+weather_data = requests.get(url)
+
+if weather_data.status_code == 200:
+    data = weather_data.json()
+    for k,v in data.items():
+        print(k, v)
+
