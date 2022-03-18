b9c53f0ed81e05765b869c409a82ecb6517c9886
Albert Norman <albertnorman@gmail.com>
2022-03-18 12:10:30 -0230

    Exception examples added

---
+++ Other Examples - Exceptions/exception_with_finally.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate finally
+# Exception raised in try block
+try:
+    k = 5//0  # raises divide by zero exception.
+    print(k)
+
+# handles zerodivision exception
+except ZeroDivisionError:
+    print("Can't divide by zero")
+
+finally:
+    # this block is always executed
+    # regardless of exception generation.
+    print('This is always executed')
