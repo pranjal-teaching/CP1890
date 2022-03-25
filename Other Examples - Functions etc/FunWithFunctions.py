a706f5051afc7694d8ddd2c87aad3caf15b58f5b
Albert Norman <albert.norman@cna.nl.ca>
2022-03-25 08:54:53 -0230

    Function example

---
+++ Other Examples - Functions etc/FunWithFunctions.py
@@ -0,0 +1,46 @@
+#Functions are first-class objects - they can be passed around and used as arguments
+def say_hello(name):
+    return f"Hello {name}"
+
+def be_awesome(name):
+    return f"Yo {name}, together we are the awesomest!"
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
+
+print(greet_bob(say_hello))
+print(greet_bob(be_awesome))
+
+
+
+#functions can contain other functions (inner functions)
+def parent():
+    print("Printing from the parent() function")
+
+    def first_child():
+        print("Printing from the first_child() function")
+
+    def second_child():
+        print("Printing from the second_child() function")
+
+    second_child()
+    first_child()
+
+parent()
+
+
+#You can use functions as return values
+def parent(num):
+    def first_child():
+        return "Hi, I am Emma"
+
+    def second_child():
+        return "Call me Liam"
+
+    if num == 1:
+        return first_child()
+    else:
+        return second_child()
+
+parentstr = parent(1)
+print(parentstr)
